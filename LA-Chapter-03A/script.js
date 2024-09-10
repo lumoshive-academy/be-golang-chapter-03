aaf85c2779acf370689c905f3e5817631c03f2cc
lumoshive-academy <lumoshive.academy@gmail.com>
2024-09-10 13:32:13 +0700

    add chapter 03A

---
+++ LA-Chapter-03A/script.js
@@ -0,0 +1,35 @@
+// Menampilkan pesan di konsol
+console.log('Hello, World!');
+
+// Mengubah konten elemen HTML dengan id "demo"
+// document.getElementById("demo").innerHTML = "Hello, JavaScript!";
+
+// Menggunakan var 
+var x = 5;
+var y = 10;
+
+// Menggunakan let (dapat diubah)
+let z = 15;
+
+// Menggunakan const (tidak dapat diubah)
+const w = 20;
+
+// Angka
+let num = 42;
+
+// String
+let str = "Hello, world!";
+
+// Boolean
+let bool = true;
+
+// Array
+let arr = [1, 2, 3, 4, 5];
+
+// Objek
+let obj = {name: "John", age: 30};
+
+// Menampilkan pesan di konsol
+console.log(x);
+
+
